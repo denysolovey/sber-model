e1212b9fabf642a601318c360734cedbb2f42cf6
denysolovey <denis.solovej04@gmail.com>
2026-07-10 14:40:57 +0300

    Enhance API to support file upload for predictions
    Updated the FastAPI application to handle file uploads for predictions, including reading CSV data and returning prediction results.

--- app.py
+++ app.py
@@ -1,18 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+import pandas as pd
 import pickle
+import io
+
+import model
 
 app = FastAPI()
 
 with open("default_model.pkl", "rb") as f:
-    model = pickle.load(f)
+    MODEL = pickle.load(f)
+
 
 @app.get("/")
-def root():
-    return {"status": "ok"}
+def home():
+    return {
+        "status": "ok",
+        "message": "Модель кредитного скоринга работает"
+    }
+
 
 @app.post("/predict")
-def predict(data: dict):
-    return {
-        "message": "API работает",
-        "received": data
-    }
+async def predict(file: UploadFile = File(...)):
+
+    contents = await file.read()
+
+    df = pd.read_csv(io.BytesIO(contents))
+
+    probs = MODEL.predict(df)
+
+    result = []
+
+    for i in range(len(df)):
+
+        result.append({
+
+            "company": i + 1,
+
+            "pd_curve": probs[i].tolist(),
+
+            "max_pd": float(probs[i][-1])
+
+        })
+
+    return result
